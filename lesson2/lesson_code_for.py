b7944846daf4a8368451252628c3f806a39a8bca
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-15 16:06:03 +0200

    lesson 2 upd 2

---
+++ lesson2/lesson_code_for.py
@@ -0,0 +1,40 @@
+# fruits = ['mangustin', 'salak', 'passionfruit', 'dragonfruit']
+# for fruit in fruits:
+#     print(f"Chars in {fruit} is {len(fruit)}")
+#
+# print("Bye")
+
+# fruits = ['mangustin', 'salak', 'passionfruit', 'dragonfruit']
+# for i, fruit in enumerate(fruits):
+#     print(f"Chars in {fruit} at index {i} is: {len(fruit)}")
+#
+# print("Bye")
+
+
+word = "Hello world"
+# for char in word:
+#     print(char)
+
+# vowel = ['a', 'e', 'i', 'o', 'u']
+# for char in word:
+#     if char not in vowel:
+#         print(char)
+
+
+# for i in range()
+my_range = range(10)
+# print(type(my_range))
+# print(my_range)
+# print(3 in my_range)
+# print(30 in my_range)
+
+for i in range(1_000_000):
+    print(i)
+
+# for i in range(1000, 1_000_000, 100):
+#     print(i)
+
+# for i in range(1000, 100, -100):
+#     print(i)
+
+
--- lesson2/plus_mul_in_ops_on_list_str.py
+++ lesson2/plus_mul_in_ops_on_list_str.py
@@ -6,6 +6,7 @@ names = ['moshe', 'david', 'orly']
 # print(ret_val)
 # ret_val = names.index('aaa')
 # print(ret_val)
+# 'moshe' not in names
 
 
 text = "Hello, my name is Valeria and I like AI"
@@ -18,13 +19,76 @@ text = "Hello, my name is Valeria and I like AI"
 # print(text.find("apple"))
 
 # countries and cities
-countries = ["Israel", "UK", "Ukraine", "US"]
-cities = [["Tel Aviv", "Haifa", "Yerucham", "Petah Tikva"],
-          ["London", "Manchester"],
-          ["Kyiv", "Cherson"],
-          ["New York", "Los Angeles", "Las Vegas"]]
+# countries = ["Israel", "UK", "Ukraine", "US"]
+# cities = [["Tel Aviv", "Haifa", "Yerucham", "Petah Tikva"],
+#           ["London", "Manchester"],
+#           ["Kyiv", "Cherson"],
+#           ["New York", "Los Angeles", "Las Vegas"]]
+# state=input("enter state")
+# city=input("enter city")
+#
+# if state in countries:
+#     index=countries.index(state)
+#     if city in cities[index]:
+#         print (f"the city {city} is in {state}")
+#     else:
+#         print (f"the city {city} is not in {state}")
+# else:
+#     print("The country is not in the list")
 
 
 # + *
+# print(6+7)
+# print(6*7)
+# print("apple" + "pear")
+# word1 = "apple"
+# word2 = "pear"
+# word3 = word1 + word2
+# word3 = "".join([word1, word2])
+# word3 = f"{word1}{word2}"
+# print(word1, word2, word3)
+# word3 = word1 * word2
+# word3 = word1 * 3
+# print(word3)
+
+fruits = ['mangustin', 'salak', 'passionfruit', 'dragonfruit']
+vegs = ['tomato', 'cucumber', 'onion']
+# new_list = fruits + vegs
+# print(new_list)
+# new_list = fruits * vegs
+# new_list = fruits * 2
+
+# new_list = fruits + vegs # a new list is created
+# fruits.extend(vegs) # no new list! fruits is updated
+# print(new_list)
+# print(fruits)
+# print(id(new_list), id(fruits))
+# print(new_list is fruits)
+# print(id(new_list) == id(fruits))
+
+# print(result)
 
 # python typing
+# num = 5
+# num: int = 5
+# num = "tttt"
+# print(num)
+
+# countries: list = ["Israel", "UK", "Ukraine", "US"]
+# cities: list = [["Tel Aviv", "Haifa", "Yerucham", "Petah Tikva"],
+#           ["London", "Manchester"],
+#           ["Kyiv", "Cherson"],
+#           ["New York", "Los Angeles", "Las Vegas"]]
+#
+# state: str = input("enter state")
+# city: str = input("enter city")
+#
+# if state in countries:
+#     index: int = countries.index(state)
+#     if city in cities[index]:
+#         print(f"the city {city} is in {state}")
+#     else:
+#         print(f"the city {city} is not in {state}")
+#     # index = input("dsfsdf")
+# else:
+#     print("The country is not in the list")
