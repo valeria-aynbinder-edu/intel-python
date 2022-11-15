d9825fa0a23bcef8e6253b55b839907cf928fa9b
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-15 13:23:16 +0200

    lesson 2

---
+++ lesson2/lesson_code.py
@@ -0,0 +1,151 @@
+int_num = 4
+float_num = 4.0
+float_num = 4.456
+word = "Summer"
+my_bool = True # False
+my_list = [98, 89, 90]
+l1 = ["Moshe", "Eran", "Orly"]
+l2 = [90, True, 'Valeria', 5.6, 578]
+
+# + - / *
+# % //
+# print(45 % 10)
+# print(45 // 10)
+movies_len = 136
+# print(f"{136 // 60}:{136 % 60}")
+# print("{136 // 60}:{136 % 60}")
+# num = 6
+# num1 = 5.6
+# num2 = 6.0
+#
+# print(136 // 60)
+# print(136 / 60)
+# print(136 // 140)
+
+# logical ops
+# or and not
+# and: True and True => True
+# or: False and False => False
+# temperature = 25
+# is_raining = True
+# is_summer = not is_raining and temperature >= 30
+# print(is_summer)
+
+# > < >= <= == !=
+# 5 == 6
+# print("Valeria" != "valeria")
+# print(5 < "apple")
+# print(5 < True)
+# print(5.6 < 6)
+# print(None < True)
+# l = []
+# is_winter = None
+# num1 = 56
+# num2 = 476913456193459813745691364598137456
+# print("apple" < "banana")
+# print("aa" < "ab")
+# print("aa" < 3)
+
+# assignment
+# num = 56
+
+text = "Hello World!"
+# print(text[3])
+# print(text[2:7])
+# print(text[2:])
+# print(text[:10])
+# print(text[2:10:2])
+# print(text[-1])
+# print(text[::-1])
+
+grades = [89, 90, 100, 99, 95, 92, 90, 89, 95, 78, 99]
+# print(grades[3:5])
+# print(grades[::-1])
+# print(grades[-1])
+# print(grades[::2])
+# print(grades[1:2])
+# print(grades[1])
+# print(grades[1:2] == grades[1])
+# print(grades[1] == grades[6])
+# print([60] == 60)
+# one_elem_list = [60]
+# print(one_elem_list[0] == 60)
+# print('60' == 60) # false
+# print(int('60') == 60) #true
+# print('60' == str(60)) #true
+# print(bool(0))
+# print(bool(None))
+# print(bool(''))
+# print(bool('sd'))
+# print(bool('0'))
+# print(list(45))
+# print(list("Hello"))
+
+# my_list = [45, "hello", True, 60 == 70, 6.7, [5, 6, 7]]
+# print(my_list)
+# print(my_list[-1])
+# print(my_list[-1][-1])
+# print(my_list[1][-1])
+# inner_list = my_list[-1]
+# inner_list[-1]
+
+# my_list = [45, "hello", True, 60 == 70, 6.7,
+#            [5, 6, 7], ["Tel Aviv", "London", [100, 200, 300]]]
+# print(my_list[-1][-1][1])
+
+# coordinates = [[34.6, 67.9, 9.0],
+#                [67.1, 57.3, 8.9]]
+# print(coordinates[0][0], coordinates[1][0])
+
+# pic = [[[56, 45.6, 67]],[],
+#        [],[]]
+
+#
+# l = [['Paris', 'London', 'New York'],
+#      [45, True, 5.5, 'hello'],
+#      -3,
+#      [5, ['#', '$', '%', '^', [10, 20, 30, 40]]],
+#      [['a'], ['b'], 'c', [['d']]]]
+#
+# print(l[3][1][3])
+
+# my_text = "Hello and bye bye"
+# lower_text = my_text.lower()
+# print(lower_text)
+# print(my_text)
+# words = my_text.split(" ")
+# print(words)
+# print(my_text.split("and"))
+# print()
+# input()
+# int()
+# my_text.isdigit()
+# print("sdfsdf".isdigit())
+# print("".isdigit())
+# print("34536".isdigit())
+# print("34.536".isdigit())
+# print("valeria aynbinder".title())
+# print("vAleRIA aynbinder".title())
+# print("    Hello        ".strip())
+# print(" 56".strip() == "56")
+
+# print(words)
+# ret_val = words.append("apple")
+# print(ret_val)
+# print(words)
+# words[1] = 'pear'
+# print(words)
+# words.insert(2, 'apple')
+# print(words)
+# fruits = ['banana', 'ananas', 'mango']
+# words.extend(fruits)
+# print(words)
+# words.append(fruits)
+# print(words)
+# words.pop(-1)
+# print(words)
+# words.sort()
+# print(words)
+# words.sort(reverse=True)
+# print(words)
+
---
+++ lesson2/lesson_code_while.py
@@ -0,0 +1,80 @@
+# counter = 0
+# grades = []
+#
+# while counter < 3:
+#     grade = int(input("Insert grade: "))
+#     grades.append(grade)
+#     counter += 1
+#     # counter = counter + 1
+# print(grades)
+
+
+# c = input("Insert temperature in C: ")
+# while not c.isdigit():
+#     c = input("Please retry: ")
+#
+# c = int(c)
+# f = c * (9/5) + 32
+# print(f"{c} in C is {f} in F")
+
+# c = "abc"
+# while not c.isdigit():
+#     c = input("Insert a number: ")
+#
+# c = int(c)
+# f = c * (9/5) + 32
+# print(f"{c} in C is {f} in F")
+
+# a = 5
+# while a != 3:
+#     b =7
+#     print("fgdfgdfg")
+
+
+# invalid_input = True
+# while invalid_input:
+#     c = input("Insert temperature in C: ")
+#     if c.isdigit():
+#         invalid_input = False
+#
+# c = int(c)
+# f = c * (9/5) + 32
+# print(f"{c} in C is {f} in F")
+
+
+# while True:
+#     c = input("Insert temperature in C: ")
+#     if c.isdigit():
+#         break
+#     print("after if")
+#
+# c = int(c)
+# f = c * (9/5) + 32
+# print(f"{c} in C is {f} in F")
+
+# while True:
+#     num = input("Insert a number or $ to finish: ")
+#     if num == '$':
+#         break
+#     num = int(num)
+#
+#     if num % 10 == 0:
+#         continue
+#
+#     print("your number is", num)
+#
+# print("Bye-bye")
+
+# while iterating over lists
+grades = [98, 99, 78, 90, 100]
+i = 0
+excellent_grades_count = 0
+while i < len(grades):
+    if grades[i] >= 95:
+        print(f"Excellent grade: {grades[i]}")
+        excellent_grades_count += 1
+    i += 1
+print("Total excellent grades:", excellent_grades_count)
+print("Value of i:", i)
+print("Bye-bye")
+
