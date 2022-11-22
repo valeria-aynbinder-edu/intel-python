26f5cebc7983d0f7840325f6e807624ee75534ad
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-22 10:28:40 +0200

    lesson 3

--- lesson3/month_year.py
+++ lesson3/month_year.py
@@ -8,3 +8,28 @@ def is_31_days_month(month: int) -> bool:
 
 
 def get_days_in_month(month: int, year: int) -> int:
+    if is_31_days_month(month):
+        return 31
+    elif month == 2:
+        # return 29 if leap_year(year) else 28
+        if leap_year(year):
+            return 29
+        else:
+            return 28
+    else:
+        return 30
+
+
+print("Hello and Welcome")
+command = input("What would you like to do? 'y' for detecting leap year, 'm' for days in month")
+if command == 'y':
+    year = int(input("Insert year: "))
+    if leap_year(year):
+        print(f"leap year")
+    else:
+        print("not leap year")
+elif command == 'm':
+    year = int(input("Insert year: "))
+    month = int(input("Insert month: "))
+    days = get_days_in_month(month, year)
+    print("There are ", days," days in month")
